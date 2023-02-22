fd36ca2da85bf4c43932737eca6ecbbf5ea69b64
levi <levihsmn@gmail.com>
2023-02-22 01:08:47 +0100

    Validations

--- src/lists/Wettkampfdefinitionsliste.php
+++ src/lists/Wettkampfdefinitionsliste.php
@@ -214,37 +214,65 @@ class Wettkampfdefinitionsliste implements Liste
     {
         $string = new DSV6StringBuilder();
         $string->add('FORMAT:Wettkampfdefinitionsliste;6;');
-        $string->add('ERZEUGER:' . $this->erzeugerSoftware . ';' . $this->erzeugerSoftwareVersion . ';' . $this->erzeugerSoftwareEmail . ';');
-        $string->add('VERANSTALTUNG:' . $this->veranstaltungName . ';' . $this->veranstaltungOrt . ';' . $this->veranstaltungBahnlaenge . ';' . $this->veranstaltungZeitmessung . ';');
-        $string->add('VERANSTALTUNGSORT:' . $this->veranstaltungsortName . ';' . $this->veranstaltungsortStrasse . ';' . $this->veranstaltungsortPlz . ';' . $this->veranstaltungsortOrt . ';' . $this->veranstaltungsortLand . ';' . $this->veranstaltungsortTelefon . ';' . $this->veranstaltungsortFax . ';' . $this->veranstaltungsortEmail . ';');
-        $string->add('AUSSCHREIBUNGIMNETZ:' . $this->ausschreibungInternet . ';');
-        $string->add('VERANSTALTER:' . $this->veranstalterName . ';');
-        $string->add('AUSRICHTER:' . $this->ausrichterName . ';' . $this->ausrichterAnsprechpartner . ';' . $this->ausrichterStrasse . ';' . $this->ausrichterPlz . ';' . $this->ausrichterOrt . ';' . $this->ausrichterLand . ';' . $this->ausrichterTelefon . ';' . $this->ausrichterFax . ';' . $this->ausrichterEmail . ';');
-        $string->add('MELDEADRESSE:' . $this->meldeadresseName . ';' . $this->meldeadresseStrasse . ';' . $this->meldeadressePlz . ';' . $this->meldeadresseOrt . ';' . $this->meldeadresseLand . ';' . $this->meldeadresseTelefon . ';' . $this->meldeadresseFax . ';' . $this->meldeadresseEmail . ';');
-        $string->add('MELDESCHLUSS:' . $this->meldeschlussDatum . ';' . $this->meldeschlussZeit . ';');
+        if(isset($this->erzeugerSoftware) && isset($this->erzeugerSoftwareVersion) && isset($this->erzeugerSoftwareEmail)) {
+            $string->add('ERZEUGER:' . $this->erzeugerSoftware . ';' . $this->erzeugerSoftwareVersion . ';' . $this->erzeugerSoftwareEmail . ';');
+        }
+        if(isset($this->veranstaltungName) && isset($this->veranstaltungOrt) && isset($this->veranstaltungBahnlaenge) && isset($this->veranstaltungZeitmessung)) {
+            $string->add('VERANSTALTUNG:' . $this->veranstaltungName . ';' . $this->veranstaltungOrt . ';' . $this->veranstaltungBahnlaenge . ';' . $this->veranstaltungZeitmessung . ';');
+        }
+        if(isset($this->veranstaltungsortName) && isset($this->veranstaltungsortStrasse) && isset($this->veranstaltungsortPlz) && isset($this->veranstaltungsortOrt) && isset($this->veranstaltungsortLand) && isset($this->veranstaltungsortTelefon) && isset($this->veranstaltungsortFax) && isset($this->veranstaltungsortEmail)) {
+            $string->add('VERANSTALTUNGSORT:' . $this->veranstaltungsortName . ';' . $this->veranstaltungsortStrasse . ';' . $this->veranstaltungsortPlz . ';' . $this->veranstaltungsortOrt . ';' . $this->veranstaltungsortLand . ';' . $this->veranstaltungsortTelefon . ';' . $this->veranstaltungsortFax . ';' . $this->veranstaltungsortEmail . ';');
+        }
+        if(isset($this->ausschreibungInternet)) {
+            $string->add('AUSSCHREIBUNGIMNETZ:' . $this->ausschreibungInternet . ';');
+        }
+        if(isset($this->veranstalterName)) {
+            $string->add('VERANSTALTER:' . $this->veranstalterName . ';');
+        }
+        if(isset($this->ausrichterName) && isset($this->ausrichterAnsprechpartner) && isset($this->ausrichterStrasse) && isset($this->ausrichterPlz) && isset($this->ausrichterOrt) && isset($this->ausrichterLand) && isset($this->ausrichterTelefon) && isset($this->ausrichterFax) && isset($this->ausrichterEmail)) {
+            $string->add('AUSRICHTER:' . $this->ausrichterName . ';' . $this->ausrichterAnsprechpartner . ';' . $this->ausrichterStrasse . ';' . $this->ausrichterPlz . ';' . $this->ausrichterOrt . ';' . $this->ausrichterLand . ';' . $this->ausrichterTelefon . ';' . $this->ausrichterFax . ';' . $this->ausrichterEmail . ';');
+        }
+        if(isset($this->meldeadresseName) && isset($this->meldeadresseStrasse) && isset($this->meldeadressePlz) && isset($this->meldeadresseOrt) && isset($this->meldeadresseLand) && isset($this->meldeadresseTelefon) && isset($this->meldeadresseFax) && isset($this->meldeadresseEmail)) {
+            $string->add('MELDEADRESSE:' . $this->meldeadresseName . ';' . $this->meldeadresseStrasse . ';' . $this->meldeadressePlz . ';' . $this->meldeadresseOrt . ';' . $this->meldeadresseLand . ';' . $this->meldeadresseTelefon . ';' . $this->meldeadresseFax . ';' . $this->meldeadresseEmail . ';');
+        }
+        if(isset($this->meldeschlussZeit) && isset($this->meldeschlussDatum)) {
+            $string->add('MELDESCHLUSS:' . $this->meldeschlussDatum . ';' . $this->meldeschlussZeit . ';');
+        }
         if(isset($this->bankVerbindungName)) {
             $string->add('BANKVERBINDUNG:' . $this->bankverbindungName . ';' . $this->bankverbindungIBAN . ';' . $this->bankverbindungBIC . ';');
         }
-        $string->add('BESONDERES:' . $this->besonderes . ';');
-
-        foreach ($this->abschnitte as $abschnitt) {
-            $string->add('ABSCHNITT:' . $abschnitt->getNummer() . ';' . $abschnitt->getDatum() . ';' . $abschnitt->getEinlassZeit() . ';' . $abschnitt->getKampfRichterSitzungZeit() . ';' . $abschnitt->getBeginnZeit() . ';;');
+        if(isset($this->besonderes)) {
+            $string->add('BESONDERES:' . $this->besonderes . ';');
         }
 
-        foreach ($this->wettkaempfe as $wettkampf) {
-            $string->add('WETTKAMPF:' . $wettkampf->getNummer() . ';' . $wettkampf->getType() . ';' . $wettkampf->getAbschnittNummer() . ';' . $wettkampf->getAnzahlStarter() . ';' . $wettkampf->getStrecke() . ';' . $wettkampf->getTechnik() . ';' . $wettkampf->getAusuebung() . ';' . $wettkampf->getGeschlecht() . ';' . $wettkampf->getZuordnungBestenliste() . ';' . $wettkampf->getQualifikationsWettkampfNummer() . ';' . $wettkampf->getQualifikationsWettkampfTyp() . ';');
+        if(isset($this->abschnitte)) {
+            foreach ($this->abschnitte as $abschnitt) {
+                $string->add('ABSCHNITT:' . $abschnitt->getNummer() . ';' . $abschnitt->getDatum() . ';' . $abschnitt->getEinlassZeit() . ';' . $abschnitt->getKampfRichterSitzungZeit() . ';' . $abschnitt->getBeginnZeit() . ';;');
+            }
         }
 
-        foreach ($this->wertungen as $wertung) {
-            $string->add('WERTUNG:' . $wertung->getWettkampfNummer() . ';' . $wertung->getWertungsId() . ';' . $wertung->getWettkampfTyp() . ';' . $wertung->getMinimumAlterOderJahr() . ';' . $wertung->getMaximumAlterOderJahr() . ';' . $wertung->getGeschlecht() . ';' . $wertung->getName() . ';');
+        if(isset($this->wettkaempfe)) {
+            foreach ($this->wettkaempfe as $wettkampf) {
+                $string->add('WETTKAMPF:' . $wettkampf->getNummer() . ';' . $wettkampf->getType() . ';' . $wettkampf->getAbschnittNummer() . ';' . $wettkampf->getAnzahlStarter() . ';' . $wettkampf->getStrecke() . ';' . $wettkampf->getTechnik() . ';' . $wettkampf->getAusuebung() . ';' . $wettkampf->getGeschlecht() . ';' . $wettkampf->getZuordnungBestenliste() . ';' . $wettkampf->getQualifikationsWettkampfNummer() . ';' . $wettkampf->getQualifikationsWettkampfTyp() . ';');
+            }
         }
 
-        foreach ($this->pflichtzeiten as $pflichtzeit) {
-            $string->add('PFLICHTZEIT:' . $pflichtzeit->getWettkampfNummer() . ';' . $pflichtzeit->getWettkampfTyp() . ';' . $pflichtzeit->getMinimumAlterOderJahr() . ';' . $pflichtzeit->getMaximumAlterOderJahr() . ';' . $pflichtzeit->getPflichtzeit() . ';' . $pflichtzeit->getGeschlecht() . ';');
+        if(isset($this->wertungen)) {
+            foreach ($this->wertungen as $wertung) {
+                $string->add('WERTUNG:' . $wertung->getWettkampfNummer() . ';' . $wertung->getWertungsId() . ';' . $wertung->getWettkampfTyp() . ';' . $wertung->getMinimumAlterOderJahr() . ';' . $wertung->getMaximumAlterOderJahr() . ';' . $wertung->getGeschlecht() . ';' . $wertung->getName() . ';');
+            }
         }
 
-        foreach ($this->meldegelder as $meldegeld) {
-            $string->add('MELDEGELD:' . $meldegeld->getTyp() . ';' . $meldegeld->getBetrag() . ';' . $meldegeld->getWettkampfNummer() . ';');
+        if(isset($this->pflichtzeiten)) {
+            foreach ($this->pflichtzeiten as $pflichtzeit) {
+                $string->add('PFLICHTZEIT:' . $pflichtzeit->getWettkampfNummer() . ';' . $pflichtzeit->getWettkampfTyp() . ';' . $pflichtzeit->getMinimumAlterOderJahr() . ';' . $pflichtzeit->getMaximumAlterOderJahr() . ';' . $pflichtzeit->getPflichtzeit() . ';' . $pflichtzeit->getGeschlecht() . ';');
+            }
+        }
+
+        if(isset($this->meldegelder)) {
+            foreach ($this->meldegelder as $meldegeld) {
+                $string->add('MELDEGELD:' . $meldegeld->getTyp() . ';' . $meldegeld->getBetrag() . ';' . $meldegeld->getWettkampfNummer() . ';');
+            }
         }
 
         $string->add('DATEIENDE');
